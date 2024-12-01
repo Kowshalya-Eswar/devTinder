a895b2003988079724ec51dbc8a48a46d3e36a54
Kowshalya <kowshalya@tigeen.ch>
2024-12-01 22:25:43 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,41 @@ const express = require('express');
 
 const app = express();
 
-/*app.use((req, res) => {
-  res.send("Hello from server");
-});*/
-app.use("/test",(req, res) => {
-    res.send("Hello from server1");
+/* app.use("/user",(req, res) => {
+    res.send("general user, if we use first it overrides all the differnt get, post, delete");
+  }); */
+/* app.get("/user",(req, res) => {
+   console.log(req.query);
+    res.send("get user");
+  }); */
+  app.get("/user/:userid/:password",(req, res) => {
+    console.log(req.params);
+     res.send("get user");
+   });
+  app.post("/user",(req, res) => {
+    res.send("post user");
+  });
+  app.delete("/user",(req, res) => {
+    res.send("delete user");
   });
 
+app.use("/t(es)?t",(req, res) => {
+    res.send("test regex");
+  });
+
+/* app.use("/test",(req, res) => {
+    res.send("Hello from server1");
+  }); */
+  app.use("/hello/2",(req, res) => {
+    res.send("Hello2");
+  });
   app.use("/hello",(req, res) => {
     res.send("Hello");
   });
+
+/*   app.use("/",(req, res) => {
+    res.send("Hello from server");
+  }); */
 app.listen(7777, () =>{
-    console.log("server is successfully listening on port 3000");
+    console.log("server is successfully listening on port 7777");
 });
