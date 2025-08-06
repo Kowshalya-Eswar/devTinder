18f15fd9443b0aaf292f576bf0d749c25cd44d7a
kowshalya <kowshalya@tigeen.ch>
2025-08-06 15:28:54 +0530

    socket changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,11 +3,16 @@ const socket = require("socket.io");
 const initializeSocket = (server) => {
     const io = socket(server,{
         cors:{
-            origin: "http://localhost:5173"
+            origin: "http://localhost:5173",
+            credentials: true,
         }
     })
     io.on("connection", (socket)=>{
-        socket.on("joinChat",()=>{})
+        socket.on("joinChat",(firstName, userId, targetUserId)=>{
+            const roomId = [userId, targetUserId].sort().join("_");
+            socket.join(roomId);
+            console.log(firstName + "joined"+roomId)
+        })
         socket.on("sendMessage",()=>{})
         socket.on("disconnect",() =>{})
     })
